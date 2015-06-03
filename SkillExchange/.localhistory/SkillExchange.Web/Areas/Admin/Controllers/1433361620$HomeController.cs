4dcb686cdb5ebbfd558384ed7236c762b9c1942e
Yavor Mitev <yavor2000@yahoo.com>
2015-06-03 22:37:51 +0300

    administrator area added

---
+++ SkillExchange/.localhistory/SkillExchange.Web/Areas/Admin/Controllers/1433361620$HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace SkillExchange.Web.Areas.Admin.Controllers
+{
+    using SkillExchange.Data.Data;
+    using SkillExchange.Web.Controllers;
+
+    public class HomeController : BaseController
+    {
+        protected HomeController(ISkillExchangeData data)
+            : base(data)
+        {
+        }
+    }
+}
---
+++ SkillExchange/.localhistory/SkillExchange.Web/Areas/Admin/Controllers/1433362149$HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace SkillExchange.Web.Areas.Admin.Controllers
+{
+    using System.Web.Mvc;
+
+    using SkillExchange.Data.Data;
+    using SkillExchange.Web.Controllers;
+
+    public class HomeController : BaseController
+    {
+        protected HomeController(ISkillExchangeData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+
+            return this.View();
+        }
+    }
+}
